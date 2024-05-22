e46104d010ffd2de56669891102779ec6962d0c5
sobhan369arab <sobhanarab30@gmail.com>
2024-05-22 16:37:44 +0430

    start project

---
+++ @latest/src/main.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+
+ReactDOM.createRoot(document.getElementById('root'))
+
+  .render(
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/Home' element={<Navigate replace to='/' />}>
+          {/* <Route path='/Home' element={</>}>  */}
+
+        </Route>
+      </Routes>
+    </Router>
+
+  );
