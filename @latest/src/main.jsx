2c52cb4e3472b518302e2258179b24aeea894337
sobhan369arab <sobhanarab30@gmail.com>
2024-05-24 12:52:50 +0430

    vn

--- @latest/src/main.jsx
+++ @latest/src/main.jsx
@@ -1,20 +1,19 @@
-import React from 'react'
+// import React from 'react'
 import ReactDOM from 'react-dom/client'
-import  { Home } from './App.jsx'
-import './index.css'
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { Home } from './Home.jsx';
+import { AllProduct } from './components/AllProduct.jsx';
+import { Menu } from './components/Menu.jsx';
 
 ReactDOM.createRoot(document.getElementById('root'))
 
   .render(
     <Router>
       <Routes>
+        <Route path='/Home' element={<Navigate replace to='/' />} />
+        <Route path='/Menu' element={<Menu />} />
+        <Route path='/AllProduct' element={<AllProduct />} />
         <Route path='/' element={<Home />} />
-        <Route path='/Home' element={<Navigate replace to='/' />}>
-          {/* <Route path='/Home' element={</>}>  */}
-
-        </Route>
       </Routes>
     </Router>
-
   );
